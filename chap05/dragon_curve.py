ec3d852230837fce9af33bcf1b421d4ae4f4ea75
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2013-10-01 19:58:52 -0400

    Adding dragon curve

---
+++ chap05/dragon_curve.py
@@ -0,0 +1,31 @@
+from swampy.TurtleWorld import *
+
+length = 5
+def dragon_curve(t, n):
+    def x_func(t, n):
+        if n == 0:
+            return
+        else:
+            x_func(t, n-1)
+            rt(t, 90)
+            y_func(t, n-1)
+            fd(t, length)
+
+    def y_func(t, n):
+        if n == 0:
+            return
+        else:
+            fd(t, length)
+            x_func(t, n-1)
+            lt(t, 90)
+            y_func(t, n-1)
+    
+    fd(t, length)
+    x_func(t, n)
+
+
+
+world = TurtleWorld()
+bob = Turtle()
+bob.delay = 0.01
+dragon_curve(bob, 10)
