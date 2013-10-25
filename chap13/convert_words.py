8f47dfadca87b6c549bb16ac28a41df2cfd208ef
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2013-10-25 12:16:48 -0400

    Adding Chapter 13 exercises.

---
+++ chap13/convert_words.py
@@ -0,0 +1,16 @@
+import string
+
+def convert_words(filename):
+    fin = open(filename)
+    s = string.whitespace + string.punctuation
+    
+    w_list = []
+    for line in fin:
+        for c in s:
+            if c in line:
+                w = line.replace(c, '')
+                word = w.strip()
+        w_list.append(word.lower())
+    return w_list
+
+print convert_words('strings.txt')
